299c282504ebf4fca59f374af6ea4a12de09144a
jkeatpri <108395205+jkeatpri@users.noreply.github.com>
2022-07-18 18:30:34 +0800

    Milestone 1: Use Intent

---
+++ app/src/main/java/com/example/messagekeatprichar/MessageActivity.java
@@ -0,0 +1,23 @@
+package com.example.messagekeatprichar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.messagekeatprichar.MESSAGE";
+
+    @Override
+    protected  void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView tvMessage = findViewById(R.id.tvMessage);
+        tvMessage.setText(message);
+    }
+}
