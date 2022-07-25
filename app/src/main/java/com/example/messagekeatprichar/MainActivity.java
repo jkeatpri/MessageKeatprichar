042f609276689fdc84d8758359ab88801b7f472d
jkeatpri <108395205+jkeatpri@users.noreply.github.com>
2022-07-25 16:54:10 +0800

    Milestone 5: Use preferences

--- app/src/main/java/com/example/messagekeatprichar/MainActivity.java
+++ app/src/main/java/com/example/messagekeatprichar/MainActivity.java
@@ -5,27 +5,87 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PREF_NIGHT = "NIGHT";
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SharedPreferences preferences = getSharedPreferences("Keatprichar", Activity.MODE_PRIVATE);
+        editor = preferences.edit();
+        boolean night = preferences.getBoolean(PREF_NIGHT, false);
+        initializeNightMode(night);
         btnSendListenerMethod();
         btnCallListenerMethod();
         btnMapListenerMethod();
         btnWebListenerMethod();
+        swNightListenerMethod();
+    }
+
+    private void initializeNightMode(boolean night) {
+        ConstraintLayout clMain = findViewById(R.id.clMain);
+        int textviews[] = {R.id.etMessage, R.id.swNight};
+        if (night) {
+            Switch swNight = findViewById(R.id.swNight);
+            swNight.setChecked(true);
+            clMain.setBackgroundColor(Color.BLACK);
+            for (int tv : textviews) {
+                TextView view = findViewById(tv);
+                view.setTextColor(Color.WHITE);
+            }
+        } else {
+            clMain.setBackgroundColor(Color.WHITE);
+            for (int tv : textviews) {
+                TextView view = findViewById(tv);
+                view.setTextColor(Color.BLACK);
+            }
+        }
+    }
+
+    private void swNightListenerMethod() {
+        Switch swNight = findViewById(R.id.swNight);
+        swNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout clMain = findViewById(R.id.clMain);
+                int textviews[] = {R.id.etMessage, R.id.swNight};
+                if(swNight.isChecked()){
+                    editor.putBoolean(PREF_NIGHT, true);
+                    clMain.setBackgroundColor(Color.BLACK);
+                    for (int tv : textviews){
+                        TextView view = findViewById(tv);
+                        view.setTextColor(Color.WHITE);
+                    }
+                }
+                else {
+                    editor.putBoolean(PREF_NIGHT, false);
+                    clMain.setBackgroundColor(Color.WHITE);
+                    for (int tv : textviews){
+                        TextView view = findViewById(tv);
+                        view.setTextColor(Color.BLACK);
+                    }
+                }
+                editor.apply();
+            }
+        });
     }
 
     private void btnWebListenerMethod() {
--- app/src/main/java/com/example/messagekeatprichar/MessageActivity.java
+++ app/src/main/java/com/example/messagekeatprichar/MessageActivity.java
@@ -1,6 +1,11 @@
 package com.example.messagekeatprichar;
 
+import static com.example.messagekeatprichar.MainActivity.PREF_NIGHT;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MessageActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.messagekeatprichar.MESSAGE";
@@ -19,12 +25,33 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+        SharedPreferences preferences = getSharedPreferences("Keatprichar", Activity.MODE_PRIVATE);
+        boolean night = preferences.getBoolean(PREF_NIGHT, false);
+        initializeNightMode(night);
         Intent intent = getIntent();
         String message = intent.getStringExtra(EXTRA_MESSAGE);
         TextView tvMessage = findViewById(R.id.tvMessage);
         tvMessage.setText(message);
 
         btnSaveListenerMethod();
+    }
+
+    private void initializeNightMode(boolean night) {
+        ConstraintLayout clMessage = findViewById(R.id.clMessage);
+        int textviews[] = {R.id.tvLabel, R.id.tvMessage, R.id.rbGood, R.id.rbBad};
+        if (night) {
+            clMessage.setBackgroundColor(Color.BLACK);
+            for (int tv : textviews) {
+                TextView view = findViewById(tv);
+                view.setTextColor(Color.WHITE);
+            }
+        } else {
+            clMessage.setBackgroundColor(Color.WHITE);
+            for (int tv : textviews) {
+                TextView view = findViewById(tv);
+                view.setTextColor(Color.BLACK);
+            }
+        }
     }
 
     private void btnSaveListenerMethod() {
